eb3b67b4f4950ee53d2070927e6fc636c0d9f8d5
Xvier3D <goncalvesxavier33@gmail.com>
2023-02-20 03:30:34 -0300

    First Commit

---
+++ CMakeFiles/EvilRobotGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EvilRobotGame.dir/src/main.cpp.o"
+  "CMakeFiles/EvilRobotGame.dir/src/main.cpp.o.d"
+  "EvilRobotGame"
+  "EvilRobotGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EvilRobotGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[])
+{
+	
+}
